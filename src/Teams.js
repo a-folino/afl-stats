8cb8f65cce44dadc352546803c56d486ccde55dc
Anthony Folino <91862159+a-folino@users.noreply.github.com>
2023-01-30 09:55:41 +1100

    Update Teams.js

--- src/Teams.js
+++ src/Teams.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
-const Teams = ({teams, handleTeamName}) => {
+const Teams = ({teams}) => {
 
     return ( 
         <div className="teams">
             {teams.map((team) => (
-                <div key={team.id} className="team">
-                    <Link to={`/team/${team.id}`}><h2 className="hover">{team.name}</h2></Link>
-                    <h3>{team.abbrev}</h3>
-                </div>
+                <Link to={`/team/${team.id}`}>
+                    <div key={team.id} className="team">
+                        <h2>{team.name}</h2>
+                        <h3>{team.abbrev}</h3>
+                    </div>
+                </Link>
                 ))
             }
         </div>
