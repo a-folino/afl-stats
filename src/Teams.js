e0b94fc66b3d184bfbbedc36a222c5e5af7bb3ec
Anthony Folino <91862159+a-folino@users.noreply.github.com>
2023-01-30 10:02:41 +1100

    Update Teams.js

--- src/Teams.js
+++ src/Teams.js
@@ -5,8 +5,8 @@ const Teams = ({teams}) => {
     return ( 
         <div className="teams">
             {teams.map((team) => (
-                <Link to={`/team/${team.id}`}>
-                    <div key={team.id} className="team">
+                <Link to={`/team/${team.id}`} key={team.id}>
+                    <div className="team">
                         <h2>{team.name}</h2>
                         <h3>{team.abbrev}</h3>
                     </div>
